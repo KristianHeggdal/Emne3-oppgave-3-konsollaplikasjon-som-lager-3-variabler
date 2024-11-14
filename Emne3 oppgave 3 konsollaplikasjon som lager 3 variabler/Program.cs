197842306330ab2640312808fe0e10ef3681f044
KristianHeggdal <kristianheggdal@hotmail.no>
2024-11-14 20:06:26 +0100

    Add project files.

---
+++ Emne3 oppgave 3 konsollaplikasjon som lager 3 variabler/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+/*int a;
+decimal b;
+a = 5;
+b = 3;
+decimal sum = a + b;
+Console.WriteLine(sum);
+*/
+
+int a = 5;
+decimal b = 3;
+decimal sum = a + b;
+Console.WriteLine(sum);
